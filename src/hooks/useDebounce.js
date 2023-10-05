1e4eb1df1b19e8d97f037c29dcad00a1db0279e0
vovantus <vshirokov@gmail.com>
2023-10-05 17:44:26 +0200

    fields validation added, useDebounce fixed

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,12 +1,8 @@
-import { useCallback } from 'react';
 export function useDebounce(func, ms) {
-  const freezedFunction = useCallback(
-    () =>
-      setTimeout(() => {
-        console.log('debounce');
-        func.apply(arguments);
-      }, ms),
-    [],
-  );
-  return freezedFunction;
+  return function () {
+    setTimeout(() => {
+      console.log('debounce');
+      func.apply(this, arguments);
+    }, ms);
+  };
 }
--- src/pages/AddBouquet/AddBouquetPage.jsx
+++ src/pages/AddBouquet/AddBouquetPage.jsx
@@ -1,4 +1,12 @@
-import { Container, Typography, TextField, Button, Grid } from '@mui/material';
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  FormControl,
+  FormHelperText,
+} from '@mui/material';
 import { useState } from 'react';
 import api from '../../api/api';
 import { useNavigate, useLocation } from 'react-router-dom';
@@ -11,48 +19,90 @@ export default function AddBouquet() {
     description: '',
   });
 
+  const [errorFields, setErrorFields] = useState(new Map());
   const location = useLocation();
-
   const [buttonActive, setButtonActive] = useState(true);
+  const [formError, setFormError] = useState('');
 
   const setButtonActiveDelayed = useDebounce(setButtonActive, 1000);
-
   const navigate = useNavigate();
 
   const editBouquetField = (e) => {
     const { name, value } = e.target;
+    const newFields = new Map(errorFields);
+    newFields.delete(name);
+    setErrorFields(newFields);
     setBouquet({
       ...bouquet,
       [name]: value,
     });
   };
 
+  const editPriceField = (e) => {
+    e.target.value = e.target.value.replace(/[^0-9.]/g, '');
+    editBouquetField(e);
+  };
+
+  const verifyName = (name) => {
+    if (name == '') {
+      const newFields = new Map(errorFields);
+      newFields.set('name', 'Please enter name for bouquet');
+      setErrorFields(newFields);
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const verifyPrice = (price) => {
+    if (!price.match(/^\d+$|^\d+\.\d+$|^\d+\.$/)) {
+      setErrorFields((errorFields) => {
+        const newFields = new Map(errorFields);
+        newFields.set('price', 'Enter correct price');
+        return newFields;
+      });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const verifyFields = (bouquet) => {
+    const verificationFunctions = [
+      verifyName(bouquet.name),
+      verifyPrice(bouquet.price),
+    ];
+
+    return verificationFunctions.every((el) => el === true);
+  };
+
   const saveBouquet = (e) => {
     e.preventDefault();
-    const data = {
-      Name: bouquet.name,
-      Price: bouquet.price,
-      Description: bouquet.description,
-      Visibility: true,
-    };
-    setButtonActive(false);
-    api
-      .createBouquet(data)
-      .then((result) => {
-        setButtonActiveDelayed(true);
-        setBouquet({
-          name: '',
-          price: '',
-          description: '',
+    if (verifyFields(bouquet)) {
+      const data = {
+        Name: bouquet.name,
+        Price: bouquet.price,
+        Description: bouquet.description,
+        Visibility: true,
+      };
+      setButtonActive(false);
+      api
+        .createBouquet(data)
+        .then((result) => {
+          setButtonActiveDelayed(true);
+          setBouquet({
+            name: '',
+            price: '',
+            description: '',
+          });
+          navigate('/proflorist/');
+          console.log(result);
+        })
+        .catch((e) => {
+          setFormError('Something wrong, reload page and try again');
+          console.log(e);
         });
-        navigate('/proflorist/');
-        console.log(result);
-      })
-      .catch((e) => {
-        setButtonActiveDelayed(true);
-        console.log(e);
-      });
-    console.log(bouquet);
+    }
   };
 
   console.log(location);
@@ -62,56 +112,63 @@ export default function AddBouquet() {
       <Typography variant="h4" gutterBottom>
         Add new bouquet
       </Typography>
-      <form onSubmit={saveBouquet}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              id="bouquet-name"
-              label="Name"
-              name="name"
-              variant="outlined"
-              onChange={editBouquetField}
-              value={bouquet.name}
-            />
+      <FormControl error={formError ? true : false}>
+        <form onSubmit={saveBouquet}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                error={errorFields.has('name')}
+                helperText={errorFields.get('name')}
+                fullWidth
+                id="bouquet-name"
+                label="Name"
+                name="name"
+                variant="outlined"
+                onChange={editBouquetField}
+                value={bouquet.name}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                error={errorFields.has('price')}
+                helperText={errorFields.get('price')}
+                fullWidth
+                id="bouquet-price"
+                label="Price"
+                name="price"
+                variant="outlined"
+                onChange={editPriceField}
+                value={bouquet.price}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                multiline
+                rows={4}
+                id="bouquet-description"
+                label="Description"
+                name="description"
+                variant="outlined"
+                onChange={editBouquetField}
+                value={bouquet.description}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={!buttonActive}
+              >
+                Save
+              </Button>
+              <FormHelperText>{formError}</FormHelperText>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              id="bouquet-price"
-              label="Price"
-              name="price"
-              variant="outlined"
-              onChange={editBouquetField}
-              value={bouquet.price}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              multiline
-              rows={4}
-              id="bouquet-description"
-              label="Description"
-              name="description"
-              variant="outlined"
-              onChange={editBouquetField}
-              value={bouquet.description}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              fullWidth
-              type="submit"
-              variant="contained"
-              color="primary"
-              disabled={!buttonActive}
-            >
-              Save
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+        </form>
+      </FormControl>
     </Container>
   );
 }
